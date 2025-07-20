c1e0996221dbff8014cf0365223aa1074a73b3af
yussufsamir <yussufsamir18@gmail.com>
2025-07-20 03:28:45 -0700

    task 0

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,8 +2,9 @@ from relationship_app import models
 
 
 def get_books_by_author(author_name):
+    author=models.Author.objects.get(name=author_name)
+    return author.objects.filter(author=author)
     
-    return models.Book.objects.filter(author__name=author_name)
 def list_all_books_in_library(library_name):
 
     library = models.Library.objects.get(name=library_name)
