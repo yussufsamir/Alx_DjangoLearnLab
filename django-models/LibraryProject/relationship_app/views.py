940e72550d681d9dd350db752d8bb8de553b12cb
yussufsamir <yussufsamir18@gmail.com>
2025-07-21 12:59:53 -0700

    task 1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from . models import Author, Book
 # Create your views here.
+
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+    output = "\n".join([f"{book.title} by {book.author.name}" for book in books])
+    return HttpResponse(output, content_type="text/plain")
+
