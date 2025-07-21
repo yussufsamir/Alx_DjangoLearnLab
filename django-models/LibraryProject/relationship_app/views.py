8337eb9cfb8b4d070ba5d8d560076333465ecddb
yussufsamir <yussufsamir18@gmail.com>
2025-07-21 13:16:13 -0700

    task1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from . models import Author, Book
+from django.views.generic.detail import DetailView
+from .models import Library
 # Create your views here.
 
 def list_books(request):
@@ -8,3 +10,12 @@ def list_books(request):
     output = "\n".join([f"{book.title} by {book.author.name}" for book in books])
     return HttpResponse(output, content_type="text/plain")
 
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "library_detail.html"
+    context_object_name = "library"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["books"] = self.object.books.all()
+        return context
