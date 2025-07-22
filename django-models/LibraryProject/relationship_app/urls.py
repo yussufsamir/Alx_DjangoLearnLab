8a52518c126d28e6a592ce94c6ce4988149a5201
yussufsamir <yussufsamir18@gmail.com>
2025-07-22 05:14:02 -0700

    task2

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import include, path
 from .views import list_books, LibraryDetailView
+from django.urls import path
+from .views import CustomLoginView, CustomLogoutView, RegisterView
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),  # function-based view
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # class-based view
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", CustomLogoutView.as_view(), name="logout"),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("", include("relationship_app.urls")),
 ]
+
+
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 from . models import Author, Book
 from django.views.generic.detail import DetailView
 from .models import Library
+from django.views import View
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 # Create your views here.
 
 def list_books(request):
@@ -19,3 +23,19 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context["books"] = self.object.books.all()
         return context
+class CustomLoginView(LoginView):
+    template_name = "relationship_app/login.html"
+
+class CustomLogoutView(LogoutView):
+    template_name = "relationship_app/logout.html"
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, "relationship_app/register.html", {"form": form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+        return render(request, "relationship_app/register.html", {"form": form})    
