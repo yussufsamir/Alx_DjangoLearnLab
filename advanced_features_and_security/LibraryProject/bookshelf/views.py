0a5d8e1e4f617598b121f1a78415a9bf031f16cf
yussufsamir <yussufsamir18@gmail.com>
2025-07-25 15:47:32 +0300

    task1

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -9,6 +9,7 @@ from advanced_features_and_security.LibraryProject.bookshelf.models import Book
 from django.http import HttpResponseForbidden, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
+from django.contrib.auth.decorators import permission_required
 
 
 
@@ -18,6 +19,7 @@ edit_permission = Permission.objects.get(codename='can_edit', content_type=conte
 create_permission = Permission.objects.get(codename='can_create', content_type=content_type)
 editors_group.permissions.add(edit_permission, create_permission)
 
+@permission_required('bookshelf.can_edit', raise_exception=True)
 
 def book_list(request):
     books = Book.objects.all()
