96ce77b4e6f922952cf00491ba4caf78e5fe2ade
yussufsamir <yussufsamir18@gmail.com>
2025-07-28 22:21:03 +0300

    fix2

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.urls import path, include
 from api.views import BookListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', BookListView.as_view(), name='book-list'),
+    path('api/', include('api.urls')),
 ]
