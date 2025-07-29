99c57775f26154c607453b3e0261f8b2ec803047
yussufsamir <yussufsamir18@gmail.com>
2025-07-29 09:34:33 +0300

    fix

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -57,7 +57,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
+
 
 
 ROOT_URLCONF = 'api_project.urls'
