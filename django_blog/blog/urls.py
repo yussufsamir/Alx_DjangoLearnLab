4dab19528fd640b46e906d46e0e0d476de1502f2
yussufsamir <yussufsamir18@gmail.com>
2025-08-12 17:24:32 +0300

    task 2 fix

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("profile/", views.profile, name="profile"),
     path("posts/", PostListView.as_view(), name="post-list"),
-    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("posts/new/", PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("post/new/", PostCreateView.as_view(), name="post-create"),
     path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
 ]
