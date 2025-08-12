ec2efc37b47625a1d8934d9594e4bc87ce29f189
yussufsamir <yussufsamir18@gmail.com>
2025-08-12 17:21:17 +0300

    task2 f

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     path("posts/", PostListView.as_view(), name="post-list"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("posts/new/", PostCreateView.as_view(), name="post-create"),
-    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post-update"),
+    path("posts/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
     path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
 ]
