3415dab2ed1750e2f64d74bc7dd94238f5a557b5
yussufsamir <yussufsamir18@gmail.com>
2025-08-12 15:04:06 +0300

    TASK 0 FIX

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,6 +77,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'yussuf_samir',
+        'PORT': '5432'
     }
 }
 
