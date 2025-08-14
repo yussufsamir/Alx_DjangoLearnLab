08f7cf8a319c0101bda9e80a50d6d299b9b45226
yussufsamir <yussufsamir18@gmail.com>
2025-08-14 22:44:33 +0300

    fix task 3

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
     path("register/login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
-    path("posts/<int:post_id>/comments/new/", views.CommentCreateView.as_view(), name="comment-create"),
-    path("comments/<int:pk>/edit/", views.CommentUpdateView.as_view(), name="comment-update"),
-    path("comments/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment-delete"),
+    path("post/<int:pk>/comments/new/", views.CommentCreateView.as_view(), name="comment-create"),
+    path("comment/<int:pk>/update/", views.CommentUpdateView.as_view(), name="comment-update"),
+    path("comment/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment-delete"),
 ]
