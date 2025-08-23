9f0b77c1b3c41d003231f991057efb73e859f508
yussufsamir <yussufsamir18@gmail.com>
2025-08-24 00:04:53 +0300

    fix

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'accounts',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'posts',
 ]
 
 MIDDLEWARE = [
