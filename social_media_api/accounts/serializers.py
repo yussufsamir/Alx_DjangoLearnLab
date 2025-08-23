0d2f67e236cb7a02568d69dbd0275472607b3e00
yussufsamir <yussufsamir18@gmail.com>
2025-08-23 14:58:58 +0300

    fix

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,19 +2,13 @@ from rest_framework import serializers
 from django.contrib.auth import authenticate
 from models import *
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
-
+        fields = ['username', 'email', 'password']
+    
     def create(self, validated_data):
-        user = User(**validated_data)
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
+        return User.objects.create_user(**validated_data)
+
     
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
