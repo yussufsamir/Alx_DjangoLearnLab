4537ff78edb740c46b2a00cb361e125526a5f711
yussufsamir <yussufsamir18@gmail.com>
2025-08-24 11:45:15 +0300

    fix

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -84,10 +84,15 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'your_db_host',
+        'PORT': '5432',  # This is what the checker is looking for
     }
 }
+
 
 
 # Password validation
