1a26269892af271027144d4fa668ce1a6f9aa22e
yussufsamir <yussufsamir18@gmail.com>
2025-08-24 11:14:11 +0300

    fix

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
+from social_media_api.posts import permissions
 from .serializers import *
 # Create your views here.
 class RegisterView(APIView):
@@ -25,24 +27,27 @@ class LoginView(APIView):
             return Response({'token': token.key}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-User = get_user_model()
+CustomUser = get_user_model()
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def follow_user(request, user_id):
-    try:
-        target_user = User.objects.get(id=user_id)
-        request.user.following.add(target_user)
-        return Response({'message': f'You are now following {target_user.username}'})
-    except User.DoesNotExist:
-        return Response({'error': 'User not found'}, status=404)
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def unfollow_user(request, user_id):
-    try:
-        target_user = User.objects.get(id=user_id)
-        request.user.following.remove(target_user)
-        return Response({'message': f'You have unfollowed {target_user.username}'})
-    except User.DoesNotExist:
-        return Response({'error': 'User not found'}, status=404)
+    def post(self, request, user_id):
+        try:
+            target_user = CustomUser.objects.get(id=user_id)
+            request.user.following.add(target_user)
+            return Response({'message': f'You are now following {target_user.username}'})
+        except CustomUser.DoesNotExist:
+            return Response({'error': 'User not found'}, status=404)
+
+
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        try:
+            target_user = CustomUser.objects.get(id=user_id)
+            request.user.following.remove(target_user)
+            return Response({'message': f'You have unfollowed {target_user.username}'})
+        except CustomUser.DoesNotExist:
+            return Response({'error': 'User not found'}, status=404)
